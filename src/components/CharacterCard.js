1b45260982bebacc81257dc2436d384cb3012a77
siyuan9826@gmail.com <siyuan9826@gmail.com>
2020-03-21 09:31:30 +0800

    challange-3-mvp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,20 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+
+export default function CharacterCard({ url, names, gender, local, species, status }) {
+
+  return (
+    <section>
+      <div>
+        <img src={url} alt='character' />
+      </div>
+      <div>
+        <h1>Name: {names}</h1>
+        <p>Gener: {gender}</p>
+        <p>Location: {local}</p>
+        <p>Species: {species}</p>
+        <p>Status: {status}</p>
+      </div>
+    </section>
+  )
 }
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,68 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import CharacterCard from './CharacterCard';
 
-export default function SearchForm() {
- 
+
+export default function SearchForm(searchTerms, handleChange) {
+
+  const [favChar, setFavChar] = useState([]);
+  const [charTerms, setCharTerm] = useState('');
+  const [charResults, setCharResults] = useState(favChar)
+
+  const formChange = event => {
+    setCharTerm(event.target.value);
+  }
+
+
+  useEffect(() => {
+   
+    const newCharacters = () => {
+      axios
+        .get('https://rickandmortyapi.com/api/character/')
+        .then(res => {
+          console.log(res.data);
+          setFavChar(res.data.results);
+        })
+        .catch(err => {
+          console.error('Error', err);
+        });
+    };
+    console.log(favChar)
+    const results = favChar.filter(character => {
+      return character.name.toLowerCase().includes(charTerms.toLowerCase());
+    });
+
+    newCharacters();
+    setCharResults(results);
+
+  }, [charTerms]);
+
+
+
   return (
     <section className="search-form">
-     // Add a search form here
-    </section>
+
+      {charResults.map(char => (
+        <CharacterCard
+          url={char.image}
+          names={char.name}
+          gender={char.gender}
+          local={char.location.name}
+          species={char.species.name}
+          status={char.status} />
+      ))}
+
+      <form >
+        <label htmlFor="name">Search:</label>
+        <input
+          id="name"
+          type="text"
+          name="textfield"
+          placeholder="search"
+          onChange={handleChange, formChange}
+          value={searchTerms}
+        />
+
+      </form>    </section>
   );
 }
